d34bd5b48b96fb676838f957e3838ae04f7b22c8
Felipe Salazar <felipe.salazar2004@outlook.com>
2025-06-01 18:04:55 -0300

    First commit

---
+++ src/main/java/com/fiap/queimadas/monitoramentos/MonitoramentosApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.queimadas.monitoramentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/queimadas/monitoramentos/MonitoramentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.queimadas.monitoramentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
